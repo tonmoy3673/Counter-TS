05516699b30f7dc905a11184adf082e36210ae90
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-08-21 17:22:00 +0600

    :zap: set contextAPI

--- src/context/CounterContext.tsx
+++ src/context/CounterContext.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useContext, useReducer } from "react";
 
 // ======= state type =======//
 interface CounterState {
@@ -24,21 +24,45 @@ type CounterContextType = {
   reset: () => void;
 };
 
-function counterReducer(state:CounterState, action:CounterAction):CounterState | undefined {
-    if(action.type==='increment'){
-        return {count: state.count+1}
-    }
-    if (action.type ==="decrement") {
-        return {count: state.count-1}
-    }
-    if (action.type ==='reset') {
-        return{count:0}
-    }
-    return state;
+function counterReducer(
+  state: CounterState,
+  action: CounterAction
+): CounterState {
+  if (action.type === "increment") {
+    return { count: state.count + 1 };
+  }
+  if (action.type === "decrement") {
+    return { count: state.count - 1 };
+  }
+  if (action.type === "reset") {
+    return { count: 0 };
+  }
+  return state;
 }
 
-const CounterContext: React.FC = () => {
-  return <div></div>;
+const CounterContext = createContext<CounterContextType | undefined>(undefined);
+
+export const CounterProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+  const value: CounterContextType = {
+    state,
+    increment: () => dispatch({ type: "increment" }),
+    decrement: () => dispatch({ type: "decrement" }),
+    reset: () => dispatch({ type: "reset" }),
+  };
+  return (
+    <CounterContext.Provider value={value}>{children}</CounterContext.Provider>
+  );
 };
 
-export default CounterContext;
+//  =========== Custom Hook =======//
+
+export const useCounter = (): CounterContextType => {
+  const context = useContext(CounterContext);
+  if (!context) {
+    throw new Error("Did not find provider");
+  }
+  return context;
+};
