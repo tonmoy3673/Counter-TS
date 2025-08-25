3522cf6a3e086cc790fa7411e0167f32558ac828
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-08-25 14:52:49 +0600

    :art: set active in navbar link

--- src/shared/Navbar.tsx
+++ src/shared/Navbar.tsx
@@ -1,28 +1,42 @@
 import { NavLink } from "react-router";
 
+const Navbar: React.FC = () => {
+  return (
+    <>
+      {/* =========== Navbar content ======== */}
+      <nav className="py-8 flex justify-between items-center">
+        {/* =========== Band ======== */}
+        <p className="text-2xl font-bolder">Counter App</p>
 
-const Navbar : React.FC = () => {
-    return (
-        <>
-            {/* =========== Navbar content ======== */}
-           <nav className="py-8 flex justify-between items-center">
-            {/* =========== Band ======== */}
-            <p className="text-2xl font-bolder">Counter App</p>
-            
-             <ul className="mx-auto flex gap-x-8">
-                <NavLink to='/'>
-                    Home
-                </NavLink>
-                <NavLink to='/about'>
-                    About
-                </NavLink>
-                 <NavLink to='/contact'>
-                    Contact
-                </NavLink>
-            </ul>
-           </nav>
-        </>
-    );
+        <ul className="mx-auto flex gap-x-8">
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "text-blue-500 underline" : "text-gray-700"
+            }
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "text-blue-500 underline" : "text-gray-700"
+            }
+            to="/about"
+          >
+            About
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "text-blue-500 underline" : "text-gray-700"
+            }
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
